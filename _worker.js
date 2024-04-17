2b0ad9446e9b5b6d7c6e443b65af78e315b72195
haiou matou <118984258+WenhuaWu1@users.noreply.github.com>
2024-04-17 16:41:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ let addressesapi = [
 	//'https://cc.isblchh.cloudns.ch/youxian.txt?token=hello', 
 	'https://cf-workers-text2kv-abo.pages.dev/ip.txt?token=hello',
 	//'https://cf-workers-text2kv-abo.pages.dev/cs.txt?token=hello',
-	//'https://cf-workers-text2kv-abo.pages.dev/youxian.txt?token=hello',
+	'https://cf-workers-text2kv-abo.pages.dev/youxian.txt?token=hello',
 	
 	//可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
